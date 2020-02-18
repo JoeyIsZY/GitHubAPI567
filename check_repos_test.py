87cc16d560f91194b45535a08ecf5e1e9242fa9d
JoeyIsZY <1933920443@qq.com>
2020-02-18 00:20:00 -0500

    Update check_repos_test.py

--- check_repos_test.py
+++ check_repos_test.py
@@ -6,7 +6,7 @@ from check_repos import get_github_repos
 
 class RepoTestCase(unittest.TestCase):
     def test_get_github_repos(self):
-        expected ={'555': 1, 'ip_adress': 2, 'ssw555tmHogwarts2020Spring': 20, 'SSW567': 4, 'SSW810': 3, 'test': 2, 'Triangle567': 9}
+        expected ={'555': 1, 'GitHubAPI567': 7, 'ip_adress': 2, 'ssw555tmHogwarts2020Spring': 20, 'SSW567': 4, 'SSW810': 3, 'test': 2, 'Triangle567': 9}
 
         self.assertEqual(get_github_repos('JoeyIsZY'), expected)
 
