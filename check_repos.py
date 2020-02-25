e5f6a68f2c6229e4666a3287adca1051f02b0d12
JoeyIsZY <1933920443@qq.com>
2020-02-25 00:13:56 -0500

    Add files via upload

--- check_repos.py
+++ check_repos.py
@@ -3,28 +3,17 @@ Author: JoeyIsZY
 """
 
 import requests
-import json
 
 
 def get_github_repos(github_user_id):
-    repository = {}
     user_repos =requests.get(f'https://api.github.com/users/{github_user_id}/repos')
-    parsed_user_repos = json.loads(user_repos.text)
-
-    for repo in parsed_user_repos:
-        repo_name = repo["name"]
-
-        repo_commits = requests.get(f'https://api.github.com/repos/{github_user_id}/{repo_name}/commits')
-        repo_commit = json.loads(repo_commits.text)
-
-        repository[repo_name] = len(repo_commit)
-        print(f'Repo: {repo_name} Number of commits: {len(repo_commit)}')
-
-    return repository
+    return user_repos
 
 
-if __name__ == '__main__':
-    get_github_repos('JoeyIsZY')
+def get_github_repo_commits(github_user_id, repo_name):
+    repo_commits = requests.get(f'https://api.github.com/repos/{github_user_id}/{repo_name}/commits')
+    return repo_commits
+
 
 
 
--- check_repos_test.py
+++ check_repos_test.py
@@ -1,14 +1,33 @@
+import os
 import unittest
 import json
-import requests
-from check_repos import get_github_repos
+from check_repos import get_github_repos, get_github_repo_commits
+from unittest.mock import patch
 
 
 class RepoTestCase(unittest.TestCase):
     def test_get_github_repos(self):
-        expected ={'555': 1, 'GitHubAPI567': 7, 'ip_adress': 2, 'ssw555tmHogwarts2020Spring': 20, 'SSW567': 4, 'SSW810': 3, 'test': 2, 'Triangle567': 9}
+        mock_get_github_repos_patcher = patch('check_repos.get_github_repos')
+        mock_get_github_repos = mock_get_github_repos_patcher.start()
 
-        self.assertEqual(get_github_repos('JoeyIsZY'), expected)
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        with open(f'{dir_path}/repos.json') as file:
+            mock_get_github_repos.return_value = json.load(file)
+        mock_get_github_repos_patcher.stop()
+        repo = mock_get_github_repos()
+
+        mock_get_github_repo_commits_patcher = patch('check_repos.get_github_repo_commits')
+        mock_get_github_repo_commits = mock_get_github_repo_commits_patcher.start()
+
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        with open(f'{dir_path}/repos-commits.json') as file:
+            mock_get_github_repo_commits.return_value = json.load(file)
+        mock_get_github_repo_commits.stop()
+        repo_commits = mock_get_github_repo_commits()
+
+        self.assertEqual(len(repo_commits), 7)
+        self.assertEqual(repo[0].get("full_name"), 'JoeyIsZY/555')
+        self.assertEqual(repo_commits[0].get('node_id'), 'MDY6Q29tbWl0MjQxMjc2MTA5Ojg3Y2MxNmQ1NjBmOTExOTRiNDU1MzVhMDhlY2Y1ZTFlOTI0MmZhOWQ=')
 
 
 if __name__ == '__main__':
